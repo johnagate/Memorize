d08b17481d2faa8b510d0527365252dbc3a3bc9a
John Agate <johnagate@gmail.com>
2022-06-02 21:58:06 -0700

    added two more themes

--- Memorize/Theme.swift
+++ Memorize/Theme.swift
@@ -9,26 +9,48 @@ import Foundation
 
 /* likely need to create an array of these themes to pass to the view model */
 
-class Theme {
-    let name: String
-    let emojis: Array<String>
-    let numberOfPairsOfCards: Int
-    let color: String
-    
-    init(name: String, emojis: Array<String>, numberOfPairsOfCards: Int, color: String) {
-        self.name = name
-        self.emojis = emojis
-        self.numberOfPairsOfCards = numberOfPairsOfCards
-        self.color = color
+struct Theme {
+    class Theme {
+        let name: String
+        let emojis: Array<String>
+        let numberOfPairsOfCards: Int
+        let color: String
+        
+        init(name: String, emojis: Array<String>, numberOfPairsOfCards: Int, color: String) {
+            self.name = name
+            self.emojis = emojis
+            self.numberOfPairsOfCards = numberOfPairsOfCards
+            self.color = color
+        }
     }
+    var themes: Array<Theme> = []
+    
+    init() {
+        let vehicleEmojis = ["✈️", "🚀", "🚗", "🚘", "🚙", "🏎", "🛵", "🏍", "🚌", "🚐", "🚛", "🛳", "🚑", "🛩", "🚍", "🚜", "🛴", "🚊", "🚲", "🚕", "🚚", "⛴", "🚞", "🚢"]
+        let halloweenEmojis = ["💀", "👻", "🎃", "🧙‍♀️", "🐈‍⬛" ,"👹", "🕸"]
+        let animalEmojis = ["🐈", "🐶", "🐇", "🐀", "🦔", "🦝", "🐄", "🐎", "🐍", "🐸", "🐒", "", "🦖", "🐞", "🐢", "🐛", "🐺", "🐜"]
+        let sportsEmojis = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🏓", "🎳", "🏸", "🏂", "🥊", "⛸", "⛳️", "🏹"]
+        let fruitsEmojis = ["🍏", "🍐", "🍋", "🍌", "🍉", "🍇", "🍑", "🥭", "🍍", "🥥", "🥝"]
+        let computerEmojis = ["⌚️", "📱", "💻", "⌨️", "🖥", "🖨", "🖱", "🕹"]
+        
+        let vehicleTheme = Theme(name: "Vehicles", emojis: vehicleEmojis, numberOfPairsOfCards: 12, color: "red")
+        let halloweenTheme = Theme(name: "Halloween", emojis: halloweenEmojis, numberOfPairsOfCards: 6, color: "orange")
+        let animalTheme = Theme(name: "Animals", emojis: animalEmojis, numberOfPairsOfCards: 8, color: "green")
+        let sportTheme = Theme(name: "Sports", emojis: sportsEmojis, numberOfPairsOfCards: 8, color: "blue")
+        let fruitTheme = Theme(name: "Fruits", emojis: fruitsEmojis, numberOfPairsOfCards: 9, color: "purple")
+        let computerTheme = Theme(name: "Computers", emojis: computerEmojis, numberOfPairsOfCards: 9, color: "grey")
+        
+        themes = [vehicleTheme, halloweenTheme, animalTheme, sportTheme, fruitTheme, computerTheme]
+    }
+    
+    
+    
 }
 
-let vehicleEmojis = ["✈️", "🚀", "🚗", "🚘", "🚙", "🏎", "🛵", "🏍", "🚌", "🚐", "🚛", "🛳", "🚑", "🛩", "🚍", "🚜", "🛴", "🚊", "🚲", "🚕", "🚚", "⛴", "🚞", "🚢"]
-let halloweenEmojis = ["💀", "👻", "🎃", "🧙‍♀️", "🐈‍⬛" ,"👹", "🕸"]
-let animalEmojis = ["🐈", "🐶", "🐇", "🐀", "🦔", "🦝", "🐄", "🐎", "🐍", "🐸", "🐒", "", "🦖", "🐞", "🐢", "🐛", "🐺", "🐜"]
-let sportsEmojis = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🏓", "🎳", "🏸", "🏂", "🥊", "⛸", "⛳️", "🏹"]
 
-let vehicleTheme = Theme(name: "Vehicles", emojis: vehicleEmojis, numberOfPairsOfCards: 12, color: "red")
-let halloweenTheme = Theme(name: "Halloween", emojis: halloweenEmojis, numberOfPairsOfCards: 6, color: "orange")
-let animalTheme = Theme(name: "Animals", emojis: animalEmojis, numberOfPairsOfCards: 8, color: "green")
-let sportTheme = Theme(name: "Sports", emojis: sportsEmojis, numberOfPairsOfCards: 8, color: "blue")
+
+
+
+
+
+
