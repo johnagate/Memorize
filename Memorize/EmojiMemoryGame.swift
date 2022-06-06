4c0bafe7f7f7394b13d162c21739178064fb50b9
John Agate <johnagate@gmail.com>
2022-06-05 20:38:43 -0700

    working on assignment 2. added concept of theme and six themes

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
     
-    static let emojis = ["✈️", "🚀", "🚗", "🚘", "🚙", "🏎", "🛵", "🏍", "🚌", "🚐", "🚛", "🛳", "🚑", "🛩", "🚍", "🚜", "🛴", "🚊", "🚲", "🚕", "🚚", "⛴", "🚞", "🚢"]
+    static var themes = [Theme(name: "Vehicles",
+                             emojis: ["✈️", "🚀", "🚗", "🚘", "🚙", "🏎", "🛵", "🏍", "🚌", "🚐", "🚛", "🛳", "🚑", "🛩", "🚍", "🚜", "🛴", "🚊", "🚲", "🚕", "🚚", "⛴", "🚞", "🚢"],
+                             pairsOfCards: 7,
+                             color: "red"),
+                         Theme(name: "Halloween",
+                               emojis: ["💀", "👻", "🎃", "🧌", "🧙‍♀️", "🧟‍♀️", "🕸", "🕷", "🍭", "🍬"],
+                               pairsOfCards: 6,
+                               color: "organge"),
+                         Theme(name: "Food",
+                               emojis: ["🍔", "🌭", "🌮", "🌯", "🥗", "🥪", "🍕", "🍟", "🍖", "🍗", "🍜", "🧇", "🥞", "🍭", "🥜"],
+                               pairsOfCards: 5,
+                               color: "tan"),
+                         Theme(name: "Space",
+                               emojis: ["🔭", "🪐", "☄️", "🛸", "👽", "🛰", "🚀", "👩‍🚀", "👾"],
+                               pairsOfCards: 4,
+                               color: "black"),
+                         Theme(name: "Water",
+                               emojis: ["💦", "🔫", "🌊", "💧", "🐟", "🏝", "🐙", "🦈", "🚰", "⛲️", "", "🏄‍♂️", "🤿"],
+                               pairsOfCards: 5,
+                               color: "blue"),
+                         Theme(name: "Computers",
+                               emojis: ["⌚️", "📱", "💻", "⌨️", "🖥", "🖨", "🖱", "🕹", "💾", "💿", "📹", "📺", "🎙"],
+                               pairsOfCards: 8,
+                               color: "gray"),
     
-    static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
-            emojis[pairIndex]
+    ]
+    
+    static func createMemoryGame(theme: Theme) -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: theme.pairsOfCards) { pairIndex in
+            theme.emojis[pairIndex]
         }
     }
     
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame(theme: themes[1])
     
     var cards: Array<MemoryGame<String>.Card> {
         model.cards
---
+++ Memorize/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by john agate on 6/5/22.
+//
+
+import Foundation
+
+struct Theme {
+    let name: String
+    var emojis: Array<String>
+    let pairsOfCards: Int
+    let color: String
+}
